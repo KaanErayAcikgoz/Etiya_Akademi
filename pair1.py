c15a2bb08b1181b0aab3bc38c56486495de0fdb2
KaanErayAcikgoz <118383992+KaanErayAcikgoz@users.noreply.github.com>
2022-11-17 15:25:59 +0300

    Update pair1.py

--- pair1.py
+++ pair1.py
@@ -30,45 +30,4 @@ else:
 print(f"Not ortalaması: {ortalama}, Harf notu: {harf} ")
 
 
-#ek
-# #######################
-#vize = input("Vize:")
-#final = input("Final:")
- 
-#sonuc = float(vize)*(0.4) + float(final)*(0.6)
-#if sonuc>80 and sonuc<100:
-   # print("Harf Notu: AA")
-#elif sonuc>=70 and sonuc<80:
-    #print("Harf Notu: BB")
-
-#elif sonuc>=60 and sonuc<70:
-    #print("Harf Notu: CC")
-#elif sonuc>=50 and sonuc<60:
-    #print("Harf Notu: DD")
-#elif sonuc>=0 and sonuc<50:
-    #print("Harf Notu: FF")
-#print ("Ortalama") 
-#print (sonuc)
-
-########ek2##
-#print("vize Notunuzu giriniz")
-#vizeNotu=float(input())
-#print("final Notunuzu giriniz")
-#finalNotu=float(input())
-
-#ortalamanot=vizeNotu*0.4 + finalNotu*0.6
-#print(ortalamanot)
-
-#if ortalamanot <50 and ortalamanot>0:
- #   print("FF ile kaldınız")
-#elif ortalamanot>=50 and ortalamanot<60:
- #   print("DD ile geçtiniz")
-#elif ortalamanot>=60 and ortalamanot<70:
- #   print("CC ile geçtiniz")
-#elif ortalamanot>=70 and ortalamanot<80:
- #   print("BB ile geçtiniz")
-#elif ortalamanot>=80 and ortalamanot>=100:
- #   print("AA ile geçtiniz")
-#else: 
-  # print(f"Sınava girmediniz notunuz:{ortalamanot}")
 
